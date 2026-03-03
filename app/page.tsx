a65e03f76b55bd217cf348f04822d8341618178d
GabsFns <gabriel0917py@gmail.com>
2026-03-02 21:30:33 -0300

    fix: corrigindo redirencionamento de pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Sistema da Oficina 🚚
-      </h1>
-    </div>
-  );
+ redirect("/auth/register")
 }
