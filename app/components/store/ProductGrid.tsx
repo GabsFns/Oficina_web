d27269d6f08be23d964df50560cc5cde3f432814
GabsFns <gabriel0917py@gmail.com>
2026-04-01 11:39:21 -0300

    fix: componetizacao store 2.1

--- app/components/store/ProductGrid.tsx
+++ app/components/store/ProductGrid.tsx
@@ -1,6 +1,6 @@
 import ProductCard from "./ProductCard";
 import EmptyState from "./EmptyState";
-
+import { motion } from "framer-motion";
 type Props = {
   products: any[];
   onAdd: any;
@@ -12,10 +12,18 @@ export default function ProductGrid({ products, onAdd }: Props) {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8">
+    <div className="lg:col-span-10 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8">
       {products.map((p) => (
+         <motion.div 
+                      key={p.id}
+                      whileHover={{ y: -10 }}
+                      className="group relative"
+                    >
         <ProductCard key={p.id} product={p} onAdd={onAdd} />
+        </motion.div>
       ))}
     </div>
   );
 }
+
+
