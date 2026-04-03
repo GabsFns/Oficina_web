f6a629cd6bb1977d36392f931ec55874492c0c91
GabsFns <gabriel0917py@gmail.com>
2026-04-02 21:18:27 -0300

    bug: debug de deploy

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,18 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getProductById, updateProduct, deleteProduct } from "@/app/service/product.service";
 
-export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
-  const product = await getProductById(params.id);
+export async function GET(
+  _: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
+
+  const product = await getProductById(id);
+
   return NextResponse.json(product);
 }
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
   const body = await req.json();
-  const product = await updateProduct(params.id, body);
+
+  const product = await updateProduct(id, body);
+
   return NextResponse.json(product);
 }
 
-export async function DELETE(_: NextRequest, { params }: { params: { id: string } }) {
-  await deleteProduct(params.id);
-  return NextResponse.json({ success: true });
+export async function DELETE(
+  _: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
+
+  await deleteProduct(id);
+
+  return NextResponse.json({ message: "Deletado com sucesso" });
 }
